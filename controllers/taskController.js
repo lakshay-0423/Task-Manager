c8b20a7e66abec3b6ee885defdf0421680579ee3
lakshay-0423 <blakshay162@gmail.com>
2026-02-22 16:51:05 +0530

    Implement full CRUD logic for tasks

---
+++ controllers/taskController.js
@@ -0,0 +1,65 @@
+const Task = require("../models/Task");
+
+exports.createTask = async (req, res, next) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getTasks = async (req, res, next) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getTaskById = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json(task);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateTask = async (req, res, next) => {
+  try {
+    const task = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json(task);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteTask = async (req, res, next) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json({ message: "Task deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
