62dbe7d3ee7208efb172d60e37259ef30f20d863
lakshay-0423 <blakshay162@gmail.com>
2026-02-22 17:08:53 +0530

    Add global error handling middleware

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
 
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message || "Server Error"
+    });
+});
+
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
-  })
-  .catch(err => console.log(err));
+    .then(() => {
+        console.log("MongoDB Connected");
+        app.listen(process.env.PORT, () =>
+            console.log(`Server running on port ${process.env.PORT}`)
+        );
+    })
+    .catch(err => console.log(err));
