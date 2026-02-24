39e6cd8872809efbffbdb158deb019cc0c8e50b7
lakshay-0423 <blakshay162@gmail.com>
2026-02-24 15:26:02 +0530

    Add authentication routes and JWT secret

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const taskRoutes = require("./routes/taskRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
     res.status(500).json({
