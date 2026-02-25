561b980e31bae6038ea89b9a2212c3f43438023b
lakshay-0423 <blakshay162@gmail.com>
2026-02-25 19:52:29 +0530

    Protect task routes using JWT middleware

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/taskController");
+const { protect } = require("../middleware/authMiddleware");
+
+router.use(protect);
 
 router.get("/", taskController.getTasks);
 router.post("/", taskController.createTask);
