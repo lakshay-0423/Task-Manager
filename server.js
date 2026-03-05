d1e2b70ebeaff00a5a805078ceff96251b880175
lakshay-0423 <blakshay162@gmail.com>
2026-03-05 20:28:47 +0530

    Update server to use dynamic PORT for deployment

--- server.js
+++ server.js
@@ -18,11 +18,13 @@ app.use((err, req, res, next) => {
     });
 });
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB Connected");
-        app.listen(process.env.PORT, () =>
-            console.log(`Server running on port ${process.env.PORT}`)
+        app.listen(PORT, () =>
+            console.log(`Server running on port ${PORT}`)
         );
     })
     .catch(err => console.log(err));
